6e7b603cedaf842bf80fd1cd603d666a5a742a1b
AdeebIsmail <adeebi5412@gmail.com>
2020-12-28 17:18:24 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-
-import Home from './pages/home'
-import Navbar from './components/navbar'
-import {BrowserRouter} from 'react-router-dom'
-
-function App() {
-  return (
-    <div className="App">
-        <Home/>
-    </div>
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/home";
 import Navbar from "./components/navbar";
@@ -21,15 +9,13 @@ import "./index.css";
 
 function App() {
   return (
-    
     <Router>
-      <Navbar/>
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
->>>>>>> 3a6d0106ddcfb32889937efa0e9acf5bde08e178
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import {MDBNavbar, MDBNav, MDBNavbarBrand, MDBNavLink} from 'mdbreact'
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { MDBNavbar, MDBNav, MDBNavbarBrand, MDBNavLink } from "mdbreact";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default function navbar() {
-    return (
+  return (
     <Router>
-        <MDBNavbar scrolling transparent>
-            <MDBNavbarBrand href = '/home' style = {{color:"black"}}>Giftin'</MDBNavbarBrand>
+      <MDBNavbar scrolling transparent>
+        <MDBNavbarBrand href="/" style={{ color: "black" }}>
+          Giftin'
+        </MDBNavbarBrand>
 
-            <MDBNav className = "ml-auto">
-                <MDBNavLink to = '/login'>Login</MDBNavLink>
+        <MDBNav className="ml-auto">
+          <MDBNavLink to="/login">Login</MDBNavLink>
 
-                <MDBNavLink to = '/signup'>Sign Up</MDBNavLink>
-                
-            </MDBNav>
-        </MDBNavbar>
+          <MDBNavLink to="/signup">Sign Up</MDBNavLink>
+        </MDBNav>
+      </MDBNavbar>
     </Router>
-    )
+  );
 }
