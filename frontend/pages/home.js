c3030d55718444cd385f76606285cdbb2636c1b9
LeftClik <samrut.gadde@gmail.com>
2020-12-28 12:30:30 -0600

    stuff

---
+++ frontend/pages/home.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import {AppBar, Toolbar} from '@material-ui/core';
+import App from '../../src/App';
+
+
+export default function home() {
+    return (
+        <div>
+            <AppBar>
+                
+            </AppBar>
+        </div>
+    )
+}
