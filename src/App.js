9e39bd1d30bfb0a5041cf8b3abea4c08bf805a79
Kshitij Kapoor <kshitijkapoor8@gmail.com>
2020-12-28 14:10:35 -0600

    a

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Home from '../frontend/pages/home'
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-=======
         <Home/>
->>>>>>> c3030d55718444cd385f76606285cdbb2636c1b9
     </div>
   );
 }
