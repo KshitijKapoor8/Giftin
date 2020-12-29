7dda93ecf2e28fa84debfd042226e0acd89ae48a
AdeebIsmail <adeebi5412@gmail.com>
2020-12-29 12:27:49 -0600

    webscrape

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,16 +14,19 @@ import {
 } from "mdbreact";
 import { render } from "react-dom";
 
-<<<<<<< HEAD
 function LoggedIn() {
   if (localStorage.getItem("userToken") === "") {
     return (
       <MDBNavbarNav right>
         <MDBNavItem>
-          <MDBNavLink to="/login">login</MDBNavLink>
+          <MDBNavLink to="/login" style={{ fontSize: "1.4rem" }}>
+            login
+          </MDBNavLink>
         </MDBNavItem>
         <MDBNavItem>
-          <MDBNavLink to="/signup">sign up</MDBNavLink>
+          <MDBNavLink to="/signup" style={{ fontSize: "1.4rem" }}>
+            sign up
+          </MDBNavLink>
         </MDBNavItem>
       </MDBNavbarNav>
     );
@@ -47,7 +50,7 @@ function LoggedIn() {
             style={{}}
             onClick={() => {
               localStorage.setItem("userToken", "");
-              window.location.refresh();
+              window.location.reload();
               console.log(localStorage.getItem("userToken"));
             }}
           >
@@ -57,60 +60,11 @@ function LoggedIn() {
       </MDBNavbarNav>
     </>
   );
-=======
-function LoggedIn(){
-  if(localStorage.getItem("userToken") === "")
-  {
-    return(
-          <MDBNavbarNav right>
-                <MDBNavItem>     
-                  <MDBNavLink to="/login" style = {{fontSize: '1.4rem'}}>login</MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink to="/signup" style = {{fontSize: '1.4rem'}}>sign up</MDBNavLink>
-                </MDBNavItem>
-          </MDBNavbarNav>
-        
-    )
-  }
-
-  return (
-<<<<<<< HEAD
-
-  
-    <div>
-=======
-      <>
->>>>>>> 5b1ce3450595d07840c68f3082a969ccf3e9d929
-          <MDBNavbarNav left>
-              <MDBNavItem>
-                <MDBNavLink to="/wishlist">wishlist</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink to="/search">search</MDBNavLink>
-              </MDBNavItem>
-
-                          
-            </MDBNavbarNav>
-            <MDBNavbarNav right>
-                <MDBNavItem>     
-                  <MDBBtn size = "sm" color = "red" style = {{}}onClick = {() => {
-                    localStorage.setItem("userToken", "")
-                    window.location.reload();
-                    console.log(localStorage.getItem("userToken"))
-                }}>logout</MDBBtn>
-                </MDBNavItem>
-            </MDBNavbarNav>
-      </>
-      
-  )
->>>>>>> 2cd0529cc1356dcfd81c5ea07007a0bb1f233d78
 }
 export default function Navbar(props) {
   const [collapse, setCollapse] = useState(false);
   const [isWideEnough, setIsWideEnough] = useState(false);
 
-<<<<<<< HEAD
   return (
     <div>
       <header>
@@ -132,29 +86,6 @@ export default function Navbar(props) {
             <LoggedIn />
           </MDBCollapse>
         </MDBNavbar>
-=======
-    return (
-        <div>
-        <header>
-          <MDBNavbar
-            color="bg-primary"
-            fixed="top"
-            dark
-            expand="md"
-            scrolling
-            transparent
-          >
-<<<<<<< HEAD
-            <MDBNavbarBrand href="/"><strong style = {{fontSize: '1.65rem'}}>giftin'</strong></MDBNavbarBrand>
-=======
-            <MDBNavbarBrand href="/"><strong>giftin'</strong></MDBNavbarBrand>
->>>>>>> 5b1ce3450595d07840c68f3082a969ccf3e9d929
-            {!isWideEnough && (
-              <MDBNavbarToggler onClick={() => setCollapse(!collapse)} />
-            )}
-            <MDBCollapse isOpen={collapse} navbar>
-              
->>>>>>> 2cd0529cc1356dcfd81c5ea07007a0bb1f233d78
 
         {props.renderContent}
       </header>
--- src/pages/wishlist.js
+++ src/pages/wishlist.js
@@ -1,14 +1,15 @@
-import React, {useEffect, useState} from 'react'
-import Navbar from '../components/navbar'
-import {MDBView, MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
-import presentBg from '../assets/present.jpg'
-import axios from 'axios'
-import { getAmazonPrice, getAmazonTitle, getHTML } from './scrape';
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/navbar";
+import { MDBView, MDBTable, MDBTableBody, MDBTableHead } from "mdbreact";
+import presentBg from "../assets/present.jpg";
+import axios from "axios";
+import { getAmazonPrice, getAmazonTitle, getHTML } from "./scrape";
 let parsedResponse = [];
 let priceArray = [];
 let titleArray = [];
 let priceSet = [];
 let titleSet = [];
+let asin = [];
 
 export default function Wishlist() {
   if (localStorage.getItem("userToken") === "") {
@@ -16,72 +17,94 @@ export default function Wishlist() {
   }
   return <Navbar renderContent={RenderContent()} />;
 }
+function wait(ms) {
+  var start = new Date().getTime();
+  var end = start;
+  while (end < start + ms) {
+    end = new Date().getTime();
+  }
+}
 
-
-  
 function RenderContent() {
-    
-    const [loading, setLoading] = useState(true)
-    const [loadData, setLoadData] = useState(true);
-    let currentId = localStorage.getItem("userToken");
-
-    async function scrapePage() {
-        if(loading)
-            return null;
-        for(let i = 0; i < parsedResponse.length; i++)
-        {
-           
-
-            let html = await getHTML(parsedResponse[i]);
-            getAmazonPrice(html).then((res) => {
-                priceArray.push(res)
-            })
-
-            getAmazonTitle(html).then((res) => {
-                titleArray.push(res)
-            })
-            
-
-        }
-
-        priceSet = [...new Set(priceArray)];
-        titleSet = [...new Set(titleArray)];
-
-        setLoadData(false)
-                console.log(priceSet)
-
-      }
-
-    useEffect(() => {
-        axios.get(`http://localhost:5000/users/${currentId}`)
-        .then((res) => {
-            parsedResponse = JSON.parse(JSON.stringify(res.data.wishlist));
-            setLoading(false)
-        })
-        .catch((err) => {console.log(err)})
-
-    }, [])
-
-    scrapePage();
-
-    if(loading)
-    return null;
-
-        
-    return (
-            <MDBView style = {{display: 'flex', flexDirection: 'vertical', justifyContent:'center'}}src = {presentBg}>
+  const [loading, setLoading] = useState(true);
+  const [loadData, setLoadData] = useState(true);
+  const [stuff, setstuff] = useState(true);
+  let currentId = localStorage.getItem("userToken");
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/users/" + currentId)
+      .get(`http://localhost:5000/users/${currentId}`)
       .then((res) => {
         parsedResponse = JSON.parse(JSON.stringify(res.data.wishlist));
         setLoading(false);
+        amazon();
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  function amazon() {
+    for (let i = 0; i < parsedResponse.length; i++) {
+      var regex = RegExp(
+        "https://www.amazon.com/([\\w-]+/)?(dp|gp/product)/(\\w+/)?(\\w{10})"
+      );
+      var t = parsedResponse[i].match(regex);
+      if (t) {
+        asin.push(t[4]);
+      }
+    }
+    if (asin.length === parsedResponse.length) {
+      console.log(getData(asin[1]));
+    }
+
+    function getData(asin) {
+      if (asin !== "undefined") {
+        console.log(asin);
+        const API = `https://api.rainforestapi.com/request?api_key=D1353B3B816F48CDBBE7DFA7639FD47B&type=product&amazon_domain=amazon.com&asin=${asin}`;
+        return new Promise((resolve, reject) => {
+          fetch(API, {
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application///json",
+            },
+          })
+            .then((response) => {
+              response.json().then((data) => {
+                console.log(data);
+                if (data.request_info.success.toString() === "false") {
+                  console.log("hello");
+                } else if (data.request_info.success.toString() === "true") {
+                  resolve(data);
+                  getData().then((data) => {
+                    if (data.request_info.success.toString() === "false") {
+                      console.log("hello again");
+                    } else if (
+                      data.request_info.success.toString() === "true"
+                    ) {
+                      let value = data.product.buybox_winner.price.value.toString();
+                      console.log(value);
+                    }
+                  });
+                }
+              });
+            })
+            .catch((error) => {
+              console.log("Error fetching response: ", error);
+            });
+        });
+      } else {
+        console.log("wait");
+      }
+    }
+  }
+  // for (let i = 0; i < asin.length; i++) {
+  //   console.log(asin[i]);
+  //setstuff(false);
+  //console.log(getData(asin[1]));
+  // }
+
+  if (loading) return null;
 
   return (
     <MDBView
@@ -107,7 +130,6 @@ function RenderContent() {
           </h1>
         </div>
 
-<<<<<<< HEAD
         <div
           style={{
             flex: 1,
@@ -119,57 +141,28 @@ function RenderContent() {
           <MDBTable borderless>
             <MDBTableHead>
               <tr style={{ color: "white" }}>
-                <th>Link</th>
-                <th>Title</th>
-                <th>Price</th>
+                <th>#</th>
+                <th>title</th>
+                <th>price</th>
               </tr>
             </MDBTableHead>
 
-            {parsedResponse.map((index) => {
-              return (
-                <MDBTableBody key={index}>
+            <MDBTableBody>
+              {parsedResponse.map((links, index) => {
+                if (setLoadData) return null;
+
+                return (
                   <tr style={{ color: "white" }}>
-                    <th>{index}</th>
-                    <th>title</th>
+                    <th>{links}</th>
+                    <th>{priceSet[index]}</th>
                     <th>price</th>
                   </tr>
-                </MDBTableBody>
-              );
-              setReload(true);
-            })}
+                );
+              })}
+            </MDBTableBody>
           </MDBTable>
         </div>
       </div>
     </MDBView>
   );
-=======
-                                <MDBTableBody>
-                                {
-
-                                    parsedResponse.map((links, index) => {
-                                        
-                                        if(setLoadData)
-                                        return null;
-
-
-                                        return(
-                                        <tr style = {{color: 'white'}}>
-                                            <th>{links}</th>
-                                            <th>{priceSet[index]}</th>
-                                            <th>price</th>
-                                        </tr>
-                                        
-                                        )
-                                    })
-
-                                    
-                                }
-                                </MDBTableBody>
-                                          
-                        </MDBTable>
-                    </div>  
-                </div>
-            </MDBView>
-    )
->>>>>>> 2cd0529cc1356dcfd81c5ea07007a0bb1f233d78
 }
