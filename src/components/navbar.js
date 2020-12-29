97e45ba0d205b1a1a00285582f8e2fce19a26ff2
AdeebIsmail <adeebi5412@gmail.com>
2020-12-28 22:28:47 -0600

    search

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,91 +1,90 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import {
-    MDBNavbar,
-    MDBNavbarBrand,
-    MDBNavbarNav,
-    MDBNavbarToggler,
-    MDBCollapse,
-    MDBNavItem,
-    MDBNavLink,
-    MDBContainer,
-    MDBMask,
-    MDBView,
-    MDBBtn
-  } from "mdbreact";
-import { render } from 'react-dom';
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavItem,
+  MDBNavLink,
+  MDBContainer,
+  MDBMask,
+  MDBView,
+  MDBBtn,
+} from "mdbreact";
+import { render } from "react-dom";
 
-function LoggedIn(){
-  if(localStorage.getItem("userToken") === "")
-  {
-    return(
-          <MDBNavbarNav right>
-                <MDBNavItem>     
-                  <MDBNavLink to="/login">login</MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink to="/signup">sign up</MDBNavLink>
-                </MDBNavItem>
-          </MDBNavbarNav>
-        
-    )
+function LoggedIn() {
+  if (localStorage.getItem("userToken") === "") {
+    return (
+      <MDBNavbarNav right>
+        <MDBNavItem>
+          <MDBNavLink to="/login">login</MDBNavLink>
+        </MDBNavItem>
+        <MDBNavItem>
+          <MDBNavLink to="/signup">sign up</MDBNavLink>
+        </MDBNavItem>
+      </MDBNavbarNav>
+    );
   }
 
   return (
-      <>
-          <MDBNavbarNav left>
-              <MDBNavItem>
-                <MDBNavLink to="/wishlist">wishlist</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink to="/search">search</MDBNavLink>
-              </MDBNavItem>
-
-                          
-            </MDBNavbarNav>
-            <MDBNavbarNav right>
-                <MDBNavItem>     
-                  <MDBBtn size = "sm" color = "red" style = {{}}onClick = {() => {
-                    localStorage.setItem("userToken", "")
-                    window.location.reload();
-                    console.log(localStorage.getItem("userToken"))
-                }}>logout</MDBBtn>
-                </MDBNavItem>
-            </MDBNavbarNav>
-      </>
-      
-  )
+    <>
+      <MDBNavbarNav left>
+        <MDBNavItem>
+          <MDBNavLink to="/wishlist">wishlist</MDBNavLink>
+        </MDBNavItem>
+        <MDBNavItem>
+          <MDBNavLink to="/search">search</MDBNavLink>
+        </MDBNavItem>
+      </MDBNavbarNav>
+      <MDBNavbarNav right>
+        <MDBNavItem>
+          <MDBBtn
+            size="sm"
+            color="red"
+            style={{}}
+            onClick={() => {
+              localStorage.setItem("userToken", "");
+              window.location.reload();
+              console.log(localStorage.getItem("userToken"));
+            }}
+          >
+            logout
+          </MDBBtn>
+        </MDBNavItem>
+      </MDBNavbarNav>
+    </>
+  );
 }
 export default function Navbar(props) {
-  
-    const [collapse, setCollapse] = useState(false);
-    const [isWideEnough, setIsWideEnough] = useState(false);
+  const [collapse, setCollapse] = useState(false);
+  const [isWideEnough, setIsWideEnough] = useState(false);
 
-    return (
-        <div>
-        <header>
-          <MDBNavbar
-            color="bg-primary"
-            fixed="top"
-            dark
-            expand="md"
-            scrolling
-            transparent
-          >
-            <MDBNavbarBrand href="/"><strong>giftin'</strong></MDBNavbarBrand>
-            {!isWideEnough && (
-              <MDBNavbarToggler onClick={() => setCollapse(!collapse)} />
-            )}
-            <MDBCollapse isOpen={collapse} navbar>
-              
+  return (
+    <div>
+      <header>
+        <MDBNavbar
+          color="bg-primary"
+          fixed="top"
+          dark
+          expand="md"
+          scrolling
+          transparent
+        >
+          <MDBNavbarBrand href="/">
+            <strong>giftin'</strong>
+          </MDBNavbarBrand>
+          {!isWideEnough && (
+            <MDBNavbarToggler onClick={() => setCollapse(!collapse)} />
+          )}
+          <MDBCollapse isOpen={collapse} navbar>
+            <LoggedIn />
+          </MDBCollapse>
+        </MDBNavbar>
 
-              <LoggedIn/>   
-            </MDBCollapse>
-          </MDBNavbar>
-  
         {props.renderContent}
-        
-            
-        </header>
-      </div>
-    )
+      </header>
+    </div>
+  );
 }
