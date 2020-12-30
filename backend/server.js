21d6f28359d617e64332d6216f1b59a3412a7719
LeftClik <samrut.gadde@gmail.com>
2020-12-29 19:44:08 -0600

    yes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -25,4 +25,6 @@ app.use('/users', usersRouter);
 
 
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
